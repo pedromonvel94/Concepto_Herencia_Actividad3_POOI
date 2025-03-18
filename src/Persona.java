dae5b3b82958f637e217d6bc2dcd06a6b6f59d3d
Pedromonvel94 <pedro.montoya@est.iudigital.edu.co>
2025-03-17 19:55:09 -0500

    First commit: created the parent class Persona with all the attributes, the constructor method and the getters

---
+++ src/Persona.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate; //Usamos localdate para almacenar la fecha sin la hora.
+
+public class Persona {
+    private String nombre, apellido;
+    private char genero;
+    private double estatura, peso;
+    private LocalDate fechaNacimiento; // el formato que tendria al almacenar es YYYY-MM-DD
+
+    public Persona (String nombre, String apellido, char genero, double estatura, double peso, LocalDate fechaNacimiento){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.estatura = estatura;
+        this.peso = peso;
+        this. fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getApellido(){
+        return apellido;
+    }
+
+    public char getGenero(){
+        return genero;
+    }
+
+    public double getEstatura(){
+        return estatura;
+    }
+
+    public double getPeso(){
+        return peso;
+    }
+
+    public LocalDate getFechaNacimiento(){
+        return fechaNacimiento;
+    }
+
+}
